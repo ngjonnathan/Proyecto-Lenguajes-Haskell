02ff325f63855664535841db64fa80317c47727e
Jonnathan Ng <ngjonnathan@gmail.com>
2016-05-16 01:29:55 -0400

    Agregado funcion statement

--- Main.hs
+++ Main.hs
@@ -13,11 +13,9 @@ sust1 = Sust1 p (q<==>p\/r)
 sust2 :: Sust
 sust2 = Sust2 p q (r\/p) (q==>a)
 
-statement :: Term -> IO() 
-statement term = putStrLn $ showTerm term
-
-main = statement (a/\true) 
-
+main = do
+        let prueba = statement 3.1 with (p <==> q,p =: p, r) using lambda z (z)
+        print $ prueba ((p <==> q) <==> (q <==> p))
 --verify= let theorem = (true === ((p <==> p) <==> (q <==> q))) in
 --		proof theorem
 --		>>=
